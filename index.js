b9d8b2a8e2e6bbb860de0379c09a2845ecb92e27
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-04-18 14:58:21 -0700

    Completeing assignment

--- index.js
+++ index.js
@@ -15,19 +15,20 @@ let globalStore = {}
 
 // function for checking a password
 checkPassword = async (username, plaintextPassword) => {
-    // TODO: Make sure to delete this console.log once you're done implementing the function!
-    console.log('\n Uh-oh, checkPassword is not yet implemented. 😢')
     // Ensure global store contains the user 
-    // (this is a quick way to check if an object contains a key)
-    if (globalStore[username]) {
-        // TODO: Use bcrypt's compare methof to compare a plaintext password to a password hash
 
+    if (globalStore[username]) {
+        // TODO: Use bcrypt's compare method to compare a plaintext password to a password hash
+        let result = await bcrypt.compare(plaintextPassword, globalStore[username])
+        
         // TODO: The result variable is a boolean. True means the user was valid. Take action accordingly.
         if (result) {
             // TODO: Display message for valid credentials
+            console.log(`Access Granted Identity Confirmed As ${username}`)
         }
         else {
             // TODO: Display message for invalid credentials
+            console.log('You gotta bounce bruh, You do not belong here!')
         }
     }
     else {
@@ -37,14 +38,15 @@ checkPassword = async (username, plaintextPassword) => {
 }
 
 hashPassword = async (username, password) => {
-    // TODO: Make sure to delete this console.log once you're done implementing the function!
-    console.log('\nUh-oh, hashPassword is not yet implemented. 😢')
-
+    
     // TODO: Make the password hash using bcrypt
-
+    let hashedPassword = await bcrypt.hash(password, 12)
+    
     // TODO: Add the user and password hash to the global store object
-
+    globalStore[username] = hashedPassword
+    
     // TODO: Print a status update including the username and password hash
+    console.log(`Dope! User '${username}' has been added! Password is ${hashedPassword}\n`)
 }
 
 
